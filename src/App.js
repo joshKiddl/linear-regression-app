9b6b4fe24e6fa6cd0854be5a946f23dbe7158984
JoshKiddl <joshsparkes6@gmail.com>
2023-07-15 22:52:21 +0100

    UI updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import axios from 'axios';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyA9ze-yVFEIexpEfnaKBalQzYlg5fTufpI",
@@ -25,6 +24,9 @@ function App() {
   const [prediction, setPrediction] = useState('');
   const [error, setError] = useState('');
   const [waitlistMessage, setWaitlistMessage] = useState('');
+
+  const formRef = useRef(null);
+  const waitlistRef = useRef(null);
 
   const handleAgeChange = (event) => {
     setAge(event.target.value);
@@ -93,59 +95,72 @@ function App() {
     }
   };
 
+  const scrollToElement = (ref) => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <div className="container">
-      <h1 className="title">Josh's Salary Predictor</h1>
-      <h2 className="subtitle">Based on age and weight</h2>
-      <h3 className="blurb">This is a Machine Learning experiment utilizing the Linear Regression statistical modeling technique</h3>
-      <div className="form">
-        <input
-          type="text"
-          className="form-input"
-          placeholder="Age"
-          value={age}
-          onChange={handleAgeChange}
-        />
-        <br />
-        <input
-          type="text"
-          className="form-input"
-          placeholder="Weight (kg)"
-          value={weight}
-          onChange={handleWeightChange}
-        />
-        <br />
-        <button className="btn" onClick={handlePrediction}>
-          Predict Salary
-        </button>
-        <button className="btn reset-btn" onClick={handleReset}>
-          Reset
-        </button>
+      <div className="header-section">
+        <h1 className="title">Josh's Salary Predictor</h1>
+        <h2 className="subtitle">Based on age and weight</h2>
+        <h3 className="blurb">This is a Machine Learning experiment utilizing the Linear Regression statistical modeling technique</h3>
+        
       </div>
-      {error && <p className="error">{error}</p>}
-      {prediction && <p className="prediction">The predicted salary is: {prediction} each year</p>}
-      <div className="email-form">
-        <h4 className="h4">Join the Waitlist</h4>
-        <form onSubmit={handleEmailSubmission}>
-          <div>
+      <div className="content-section">
+        <div className="form-section" ref={formRef}>
+          <div className="form">
             <input
               type="text"
               className="form-input"
-              placeholder="Email"
-              value={email}
-              onChange={handleEmailChange}
+              placeholder="Age"
+              value={age}
+              onChange={handleAgeChange}
             />
+            <br />
+            <input
+              type="text"
+              className="form-input"
+              placeholder="Weight (kg)"
+              value={weight}
+              onChange={handleWeightChange}
+            />
+            <br />
+            <button className="btn" onClick={handlePrediction}>
+              Predict Salary
+            </button>
+            <button className="btn reset-btn" onClick={handleReset}>
+              Reset
+            </button>
           </div>
-          <button type="submit" className="btn submit-btn">
-            Submit
-          </button>
-        </form>
-        {waitlistMessage && <p className="success-message">{waitlistMessage}</p>}
+          {error && <p className="error">{error}</p>}
+          {prediction && <p className="prediction">The predicted salary is: {prediction} each year</p>}
+        </div>
+        <div className="waitlist-section" ref={waitlistRef}>
+          <div className="email-form">
+            <h4 className="h4">Join the Waitlist</h4>
+            <form onSubmit={handleEmailSubmission}>
+              <div>
+                <input
+                  type="text"
+                  className="form-input"
+                  placeholder="Email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <button type="submit" className="btn submit-btn">
+                Submit
+              </button>
+            </form>
+            {waitlistMessage && <p className="success-message">{waitlistMessage}</p>}
+          </div>
+        </div>
       </div>
       <footer className="footer">© 2023 Your Company. All rights reserved.</footer>
     </div>
-
   );
 }
 
