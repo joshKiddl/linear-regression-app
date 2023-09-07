7c5081bddd4a323df3e82afc3b33892b514493f9
JoshKiddl <joshsparkes6@gmail.com>
2023-09-07 10:30:36 +0100

    Integrations +1 and capg between text and x

--- src/pages/integrations.js
+++ src/pages/integrations.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/listOfFeatures.css";
 import githubLogo from "../images/github-logo.png";
 import jiraLogo from "../images/jira-logo.webp";
@@ -10,6 +10,8 @@ import Trello from "../images/trello.png";
 import mondayLogo from "../images/monday-logo.png";
 import basecampLogo from "../images/basecamp-logo.png";
 import AppSidebar from "../components/sidebar";
+import { db } from "../firebase";
+import { doc, getDoc, setDoc } from "@firebase/firestore";
 
 const integrationsData = [
   { title: "GitHub", logo: githubLogo },
@@ -21,10 +23,35 @@ const integrationsData = [
   { title: "Trello", logo: Trello },
   { title: "Asana", logo: asana },
   { title: "ProductBoard", logo: ProductBoard },
-
 ];
 
 const Integrations = () => {
+  const [showThanks, setShowThanks] = useState(false);
+
+  const incrementFeedback = async (title) => {
+    try {
+      const docRef = doc(db, "feedback", "Integrations");
+      const docSnapshot = await getDoc(docRef);
+
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        const currentCount = data[title] || 0;
+        await setDoc(docRef, { [title]: currentCount + 1 }, { merge: true });
+      } else {
+        await setDoc(docRef, { [title]: 1 });
+      }
+
+      setShowThanks(true); // Show the thank you message
+
+      // Optional: Hide the thank you message after a few seconds
+      setTimeout(() => {
+        setShowThanks(false);
+      }, 3000); // Adjust the time as needed
+    } catch (error) {
+      console.error("Error updating feedback:", error);
+    }
+  };
+
   return (
     <div className="lof-body">
       <AppSidebar>
@@ -38,9 +65,17 @@ const Integrations = () => {
                 className="feature-logo"
               />
               <h1 className="feature-title">{integration.title}</h1>
-              <button className="connect-button">Connect - coming soon</button>
+              <button
+                className="connect-button"
+                onClick={() => incrementFeedback(integration.title)}
+              >
+                Register Interest - coming soon
+              </button>
             </div>
           ))}
+          {showThanks && (
+            <div className="thanks-message">Thank you for your interest!</div>
+          )}
         </div>
       </AppSidebar>
     </div>
